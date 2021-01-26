5fa6dfc545d4df73916fbc8f6dda4fd41be5f234
Lucas Yoshimoto <lucasyoshimoto238@gmail.com>
2021-01-26 16:32:53 -0300

    Configurando o JAXB 2

---
+++ src/main/java/com/lucas/ObjectFactory.java
@@ -0,0 +1,71 @@
+//
+// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
+// Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
+// Gerado em: 2021.01.26 às 04:31:26 PM BRT 
+//
+
+
+package com.lucas;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the com.lucas package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.lucas
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link GetCustomerDetailResponse }
+     * 
+     */
+    public GetCustomerDetailResponse createGetCustomerDetailResponse() {
+        return new GetCustomerDetailResponse();
+    }
+
+    /**
+     * Create an instance of {@link CustomerDetail }
+     * 
+     */
+    public CustomerDetail createCustomerDetail() {
+        return new CustomerDetail();
+    }
+
+    /**
+     * Create an instance of {@link GetAllCustomerResponse }
+     * 
+     */
+    public GetAllCustomerResponse createGetAllCustomerResponse() {
+        return new GetAllCustomerResponse();
+    }
+
+    /**
+     * Create an instance of {@link GetCustomerDetailRequest }
+     * 
+     */
+    public GetCustomerDetailRequest createGetCustomerDetailRequest() {
+        return new GetCustomerDetailRequest();
+    }
+
+}
