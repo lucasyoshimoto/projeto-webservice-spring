1f68218fb3de369eef62960110dec5ee0e04c65f
Lucas Yoshimoto <lucasyoshimoto238@gmail.com>
2021-01-26 17:05:57 -0300

    WebServiceConfig Gerando WSDL

--- src/main/java/com/lucas/soap/webservices/customeradministration/soap/WebServiceConfig.java
+++ src/main/java/com/lucas/soap/webservices/customeradministration/soap/WebServiceConfig.java
@@ -4,9 +4,13 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
 
 @Configuration
 @EnableWs
@@ -18,5 +22,20 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 		messageDispatcherServlet.setApplicationContext(context);
 		messageDispatcherServlet.setTransformWsdlLocations(true);
 		return new ServletRegistrationBean(messageDispatcherServlet,"/ws/*");
+	}
+	
+	@Bean
+	public XsdSchema customerSchema() {
+		return new SimpleXsdSchema(new ClassPathResource("customer-information.xsd"));
+	}
+	
+	@Bean(name="customers")
+	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema customerSchema) {
+		DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+		definition.setPortTypeName("CustomerPort");
+		definition.setTargetNamespace("http://lucas.com");
+		definition.setLocationUri("/ws");
+		definition.setSchema(customerSchema);
+		return definition;
 	}
 }
