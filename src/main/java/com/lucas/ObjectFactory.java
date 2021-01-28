bd7dc8df6affff87182fe6844d2296d6ae8ac272
Lucas Yoshimoto <lucasyoshimoto238@gmail.com>
2021-01-28 18:13:50 -0300

    Implementando Segurança para SOAP Web Services com WS-Security

--- src/main/java/com/lucas/ObjectFactory.java
+++ src/main/java/com/lucas/ObjectFactory.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2021.01.27 às 08:28:26 PM BRT 
+// Gerado em: 2021.01.28 às 05:24:33 PM BRT 
 //
 
 
--- src/main/java/com/lucas/soap/webservices/customeradministration/soap/WebServiceConfig.java
+++ src/main/java/com/lucas/soap/webservices/customeradministration/soap/WebServiceConfig.java
@@ -1,5 +1,8 @@
 package com.lucas.soap.webservices.customeradministration.soap;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +10,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.xwss.XwsSecurityInterceptor;
+import org.springframework.ws.soap.security.xwss.callback.SimplePasswordValidationCallbackHandler;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
@@ -38,4 +44,25 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 		definition.setSchema(customerSchema);
 		return definition;
 	}
+	
+	@Bean
+	public XwsSecurityInterceptor securityInterceptor() {
+		XwsSecurityInterceptor securityInterceptor = new XwsSecurityInterceptor();
+		securityInterceptor.setCallbackHandler(callbackHandler());
+		securityInterceptor.setPolicyConfiguration(new ClassPathResource("securityPolicy.xml"));
+		return securityInterceptor;
+	}
+	
+	@Bean
+	public SimplePasswordValidationCallbackHandler callbackHandler() {
+		SimplePasswordValidationCallbackHandler handler = new SimplePasswordValidationCallbackHandler();
+		handler.setUsersMap(Collections.singletonMap("luca","123"));
+		return handler;
+	}
+
+	
+	@Override
+	public void addInterceptors(List<EndpointInterceptor> interceptors) {
+		interceptors.add(securityInterceptor());
+	}	
 }
