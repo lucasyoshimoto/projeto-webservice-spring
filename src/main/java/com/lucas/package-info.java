13799822ab6fac304f8584a34312c5847a9a48b9
Lucas <lucasyoshimoto238@gmail.com>
2021-01-29 14:36:16 -0300

    Cliente WebService SOAP - JAVA

--- src/main/java/com/lucas/package-info.java
+++ src/main/java/com/lucas/package-info.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2021.01.28 às 05:24:33 PM BRT 
+// Gerado em: 2021.01.29 às 11:41:09 AM BRT 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://lucas.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
